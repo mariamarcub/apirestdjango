c5f3581bce3e0ef9c61a8677f7d8d6a14b58b2ed
mariamarcub <marmarcub91@gmail.com>
2024-02-10 19:11:34 +0100

    vies.py y migraciones

--- patinetes/views.py
+++ patinetes/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from patinetes.models import Patinete, Alquiler, Usuario
+from patinetes.serializers import PatineteSerializer, AlquilerSerializer, UsuarioSerializer
 
-# Create your views here.
+
+class PatineteView(viewsets.ModelViewSet):
+    queryset = Patinete.objects.all()
+    serializer_class = PatineteSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class AlquilerView(viewsets.ModelViewSet):
+    queryset = Alquiler.objects.all()
+    serializer_class = AlquilerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class UsuarioView(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
