9b7867b7d813beb1e8b0e074d607d3262fc2cc89
mariamarcub <marmarcub91@gmail.com>
2024-02-26 12:08:08 +0100

    serializers

--- biblioteca/serializers.py
+++ biblioteca/serializers.py
@@ -8,10 +8,38 @@ class LibroSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url','usuario','titulo','autor','editorial','anyo_publicacion']
 
 
+
+# En el serializador de Alquiler, asegúrate de serializar correctamente los campos
+# relacionales como el libro y el usuario asociados con cada alquiler.
+# Validación de fechas: Implementa validación en el serializador para asegurarte
+# de que la fecha de inicio del alquiler sea anterior a la fecha de finalización,
+# si se proporciona una fecha de finalización.
+
 class AlquilerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Alquiler
         fields = ['url', 'libro','usuario','inicio','fin']
+        validators = [
+            #asegurarse de que no haya dos alquileres para el mismo libro
+            serializers.UniqueTogetherValidator(
+                queryset=Alquiler.objects.all(),
+                fields=['libro', 'inicio'],
+                message="Ya existe un alquiler para este libro en la fecha de inicio"
+            ),
+            serializers.UniqueTogetherValidator(
+                queryset=Alquiler.objects.all(),
+                fields=['libro', 'fin'],
+                message="Ya existe un alquiler para este libro en la fecha de finalización"
+            ),
+
+            #la fecha de inicio sea anterior a la fecha de finalización.
+            serializers.LessThanDatetime(
+                datetime_field='inicio',
+                datetime_to_compare='fin',
+                message="La fecha de inicio debe ser anterior a la fecha de finalización"
+            )
+        ]
+
 
 
 class UsuarioSerializer(serializers.HyperlinkedModelSerializer):
